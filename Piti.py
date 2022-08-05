b09266ba296bbff846867b9763c455a6bf7a2301
SadiqSD <sadiqmurtala20@gmail.com>
2022-08-05 05:38:22 +0100

    another windows

---
+++ Piti.py
@@ -0,0 +1,13 @@
+#there is litrary nothing here why? who kows
+times = 0
+def fold():
+    name = input("what is your name? ")
+    while name == "":
+        print("Entry is empty")
+        times += 1
+    else:
+        print("name recorded")
+
+fold()
+
+
